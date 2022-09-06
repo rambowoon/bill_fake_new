b735d746518ba3280353e7a504bb18bebef97a4c
Thiện <phamngocthien95@gmail.com>
2022-09-06 22:59:45 +0700

    update

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -12,6 +12,11 @@
   });
 }
 
+$('#link-login').change(function () {
+  const link = $(this).val();
+  $('#link-login-view-content').text(link);
+});
+
 $('#time').change(function () {
   const time = $(this).val();
   $('#time-view-content').text(time);
@@ -20,6 +25,11 @@ $('#time').change(function () {
 $('#money').change(function () {
   const money = $(this).val();
   $('#money-view-content').text(money);
+});
+
+$('#username').change(function () {
+  const username = $(this).val();
+  $('#username-view-content').text(username);
 });
 
 $('#banking-time').change(function () {
@@ -92,13 +102,151 @@ $('#beneficiary-name').change(function () {
 
 $('#card-number').change(function () {
   const value = $(this).val();
-  console.log(value);
   $('#card-number-view-content').text(value);
+});
+$('#bank').change(function () {
+  const value = $(this).val();
+  $('#bank-view-content').text(value);
+});
+$('#code').change(function () {
+  const value = $(this).val();
+  $('#code-view-content').text(value);
+});
+$('#score').change(function () {
+  const value = $(this).val();
+  $('#score-view-content').text(value);
+});
+
+$('#date').change(function () {
+  const value = $(this).val();
+  $('#date-view-content').text(value);
+});
+$('#total').change(function () {
+  const value = $(this).val();
+  $('#total-view-content').text(value);
+});
+$('#date-1').change(function () {
+  const value = $(this).val();
+  $('#date-1-view-content').text(value);
+});
+$('#money-request-1').change(function () {
+  const value = $(this).val();
+  $('#money-request-1-view-content').text(value);
+});
+$('#money-real-1').change(function () {
+  const value = $(this).val();
+  $('#money-real-1-view-content').text(value);
+});
+$('#bank-1').change(function () {
+  const value = $(this).val();
+  $('#bank-1-view-content').text(value);
+});
+$('#type-1').change(function () {
+  const value = $(this).val();
+  $('#type-1-view-content').text(value);
+});
+$('#account-bank-1').change(function () {
+  const value = $(this).val();
+  $('#account-bank-1-view-content').text(value);
+});
+$('#note-1').change(function () {
+  const value = $(this).val();
+  $('#note-1-view-content').text(value);
+});
+$('#txn-1').change(function () {
+  const value = $(this).val();
+  $('#txn-1-view-content').text(value);
+});
+
+$('#date-2').change(function () {
+  const value = $(this).val();
+  $('#date-2-view-content').text(value);
+});
+$('#money-request-2').change(function () {
+  const value = $(this).val();
+  $('#money-request-2-view-content').text(value);
+});
+$('#money-real-2').change(function () {
+  const value = $(this).val();
+  $('#money-real-2-view-content').text(value);
+});
+$('#bank-2').change(function () {
+  const value = $(this).val();
+  $('#bank-2-view-content').text(value);
+});
+$('#type-2').change(function () {
+  const value = $(this).val();
+  $('#type-2-view-content').text(value);
+});
+$('#account-bank-2').change(function () {
+  const value = $(this).val();
+  $('#account-bank-2-view-content').text(value);
+});
+$('#note-2').change(function () {
+  const value = $(this).val();
+  $('#note-2-view-content').text(value);
+});
+$('#txn-2').change(function () {
+  const value = $(this).val();
+  $('#txn-2-view-content').text(value);
+});
+
+$('#date-3').change(function () {
+  const value = $(this).val();
+  $('#date-3-view-content').text(value);
+});
+$('#type-3').change(function () {
+  const value = $(this).val();
+  $('#type-3-view-content').text(value);
+});
+$('#money-request-3').change(function () {
+  const value = $(this).val();
+  $('#money-request-3-view-content').text(value);
+});
+$('#money-real-3').change(function () {
+  const value = $(this).val();
+  $('#money-real-3-view-content').text(value);
+});
+$('#bank-3').change(function () {
+  const value = $(this).val();
+  $('#bank-3-view-content').text(value);
+});
+$('#type-3').change(function () {
+  const value = $(this).val();
+  $('#type-3-view-content').text(value);
+});
+$('#account-bank-3').change(function () {
+  const value = $(this).val();
+  $('#account-bank-3-view-content').text(value);
+});
+$('#note-3').change(function () {
+  const value = $(this).val();
+  $('#note-3-view-content').text(value);
+});
+$('#txn-3').change(function () {
+  const value = $(this).val();
+  $('#txn-3-view-content').text(value);
+});
+
+$('#date-4').change(function () {
+  const value = $(this).val();
+  $('#date-4-view-content').text(value);
+});
+$('#type-4').change(function () {
+  const value = $(this).val();
+  $('#type-4-view-content').text(value);
+});
+$('#money-real-4').change(function () {
+  const value = $(this).val();
+  $('#money-real-4-view-content').text(value);
+});
+$('#bank-4').change(function () {
+  const value = $(this).val();
+  $('#bank-4-view-content').text(value);
 });
 
 $('#fee').change(function () {
   const value = $(this).val();
-  console.log(value);
   $('#fee-view-content').text(value);
 });
 
@@ -137,7 +285,7 @@ $('.signal-3g img').click(function () {
 
 $('.signal-4g img').click(function () {
   let src = $(this).attr('src');
-  $('#signal-2 img').attr('src', src);
+  $('#signal-1 img').attr('src', src);
 });
 
 $('.menu li').click(function (e) { 
